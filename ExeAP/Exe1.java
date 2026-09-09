1f8120985c212debe5f6787f89bb3d1748fcce7d
Yuri-Vasconcellos <yuri.gabriel.vasconcellos@gmail.com>
2026-09-09 10:27:14 -0300

    Add files via upload

---
+++ ExeAP/Exe1.java
@@ -0,0 +1,11 @@
+public class Exe1 {
+public static void main(String[] args) {
+String nome = "Yuri Gabriel da Silva Vasconcellos";
+String ra = "06312722";
+String curso = "Análise e Desenvolvimento de Sistema";
+System.out.println("Meu nome completo é: "+nome);
+System.out.println("O meu registro acadêmico é: " + ra);
+System.out.println("O meu curso é: "+curso);
+}
+}
+//Yuri Gabriel da Silva Vasconcellos
---
+++ ExeAP/Exe10.java
@@ -0,0 +1,9 @@
+public class Exe10 {
+public static void main(String[] args) {
+
+double raio = 5;
+double area = Math.PI * Math.pow(raio,2);
+System.out.printf("A área de um circulo com raio de 5cm é: %.3f cm2", area);
+}
+}
+//Yuri Gabriel da Silva Vasconcellos
