72cafc00eaa277111adfd5c06ae2b04ea2effa97
Yuri-Vasconcellos <yuri.gabriel.vasconcellos@gmail.com>
2026-09-16 11:40:25 -0300

    Add files via upload

---
+++ java/exe AP/Exe2.java
@@ -0,0 +1,12 @@
+public class Exe2 {
+public static void main(String[] args) {
+int idade = 21;
+int altura = 175;
+String status = "Presente";
+
+System.out.println("Minha idade é: "+idade);
+System.out.println("Minha altura é: "+ altura+"cm");
+System.out.println("Meu status na aula é: "+status);
+}
+}
+//Yuri Gabriel da Silva Vasconcellos
