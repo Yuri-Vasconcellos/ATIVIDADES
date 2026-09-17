4f48791f5da19e4c27b656a217203cfb319b889e
Yuri-Vasconcellos <yuri.gabriel.vasconcellos@gmail.com>
2026-09-17 09:05:29 -0300

    Add files via upload

---
+++ java/exe AP/Tipos de dados/Exe7.java
@@ -0,0 +1,10 @@
+public class Exe7 {
+public static void main(String[] args) {
+double resultado = Math.pow(3, 30);
+System.out.println((120-30)==resultado);
+System.out.println(!((20%4/2)==1)||(9!=9));
+System.out.println((5%2)>3);
+System.out.println('a'=='A');
+}
+}
+//Yuri Gabriel da Silva Vasconcellos
